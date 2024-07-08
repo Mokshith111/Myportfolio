f7856630a9bf8121fb84c469c63fccbad6ca3444
Mokshith111 <120484213+Mokshith111@users.noreply.github.com>
2024-07-08 20:44:07 +0530

    Add files via upload

---
+++ portfolio/scripts.js
@@ -0,0 +1,16 @@
+function scrollToSection(sectionId) {
+    document.getElementById(sectionId).scrollIntoView({ behavior: 'smooth' });
+}
+document.getElementById('contact-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    const name = document.getElementById('name').value;
+    const email = document.getElementById('email').value;
+    const message = document.getElementById('message').value;
+
+    console.log('Form Submitted');
+    console.log('Name:', name);
+    console.log('Email:', email);
+    console.log('Message:', message);
+
+    alert('Thank you for your message!');
+})
